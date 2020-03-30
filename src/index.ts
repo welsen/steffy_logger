04264e1e24998c5e7239f0e1a8d33dddc426f0e9
Onofer Dávid <david.onofer@gmail.com>
2020-03-30 10:15:05 +0200

    change
    - add `.prettierrc`

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { injector, ILogger, useInjectionToken, registerInjectionToken, SYMBOLS } from '@steffi/core';
 import { LoggerPlugin } from './logger';
 
-export function registerLogger() {
-  registerInjectionToken('LoggerPlugin');
-  injector.bind<ILogger>(useInjectionToken('LoggerPlugin')).to(LoggerPlugin).inSingletonScope();
-}
+registerInjectionToken('LoggerPlugin');
+injector
+  .bind<ILogger>(useInjectionToken('LoggerPlugin'))
+  .to(LoggerPlugin)
+  .inSingletonScope();
 
 export * from './logger';
